54093785af21f5cc89ac60c0eb4386ac9a46ab53
Dani <daniel.alonsofernandez00@gmail.com>
2025-02-20 09:24:52 +0100

    Se añade una redireccion a un index.html

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+require("dotenv").config(); //carga las variables de entorno desde .env
+const express = require("express"); //Importa express
+const mongoose = require("mongoose");   //Importa Mongoose para conectar a mongoDB
+const cors = require("cors");   //permite peticioens desde otros dominios (CORS)
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express();  //Crea una instancia de express
+app.use(express.json());    //Permite recibir JSON en las particiones
+app.use(cors());    //Habilita CORS para permitir el acceso desde el frontend
 
-// Conectar a MongoDB
+// Conectar a MongoDB usando mogoose
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,11 +15,16 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => console.log("✅ MongoDB conectado"))
   .catch(err => console.log("❌ Error al conectar MongoDB:", err));
 
-// Ruta de prueba
+// Ruta de prueba para verificar que el servidor funciona
+app.get("/prueba",(req, res) => {res.sendFile(__dirname+'/index.html')});
+
 app.get("/", (req, res) => {
   res.send("✅ API funcionando...");
 });
+app.get("/admin",(req, res) => {res.send("Entrada para admin")});
 
-// Iniciar servidor
+// Definir el puerto del servidor (5000 por defecto o el de .env)
 const PORT = process.env.PORT || 5000;
+
+//Iniciar el servidor
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
